fd114e0797661798afadc7823b1536f5ed2d5990
Dev-Bikram <paul.bikramjitpaul1@gmail.com>
2024-02-06 14:33:29 +0530

    new update

--- pages/modal/modal.tsx
+++ pages/modal/modal.tsx
@@ -1,35 +1,35 @@
-import { Button } from '@mui/material'
-import React from 'react'
-import ShowModal from './showModal';
+// import { Button } from '@mui/material'
+// import React from 'react'
+// import ShowModal from './showModal';
 
-const Modal = () => {
+// const Modal = () => {
 
-    const[modal,setModal]= React.useState(false);
+//     const[modal,setModal]= React.useState(false);
 
-    const MyModal = ()=>{
-        return(
-            <>
-            <h2>Stay Tuned</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sapiente corrupti totam consectetur eaque nisi sunt et iure possimus, molestias magnam illo quo delectus quae quasi voluptatem porro repellendus facilis?
-            </p>
+//     const MyModal = ()=>{
+//         return(
+//             <>
+//             <h2>Stay Tuned</h2>
+//             <p>
+//                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sapiente corrupti totam consectetur eaque nisi sunt et iure possimus, molestias magnam illo quo delectus quae quasi voluptatem porro repellendus facilis?
+//             </p>
             
-            </>
-        )
-    }
+//             </>
+//         )
+//     }
 
-  return (
-    <>
+//   return (
+//     <>
     
     
-    <Button>
-        Open
-    </Button>
-    {ShowModal && <MyModal/>}
+//     <Button>
+//         Open
+//     </Button>
+//     {ShowModal && <MyModal/>}
     
     
-    </>
-  )
-}
+//     </>
+//   )
+// }
 
-export default Modal
+// export default Modal
--- pages/ratepages/index.tsx
+++ pages/ratepages/index.tsx
@@ -1,10 +1,10 @@
 import { useQuery } from 'react-query';
-import { fetchRateList} from '../../services/coinApi';
-import { Table, TableBody, TableCell, TableContainer, TableRow, Paper, Button , TablePagination } from '@mui/material';
+import { fetchRateList } from '../../services/coinApi';
+import { Table, TableBody, TableCell, TableContainer, TableRow, Paper, Button, TablePagination } from '@mui/material';
 import Link from 'next/link';
 import Wrapper from '@/Layout/wrapper';
 import React from 'react';
-import {  Dialog, DialogTitle, DialogContent, Typography, DialogActions} from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, Typography, DialogActions } from '@mui/material';
 import Grid from '@mui/material/Grid';
 
 
@@ -12,15 +12,15 @@ import Grid from '@mui/material/Grid';
 const Index: React.FC = () => {
 
   const [page, setPage] = React.useState(0);
-const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
-  
+
   const { data: rates, isLoading } = useQuery('rates', fetchRateList);
 
 
   const [selectedRate, setSelectedRate] = React.useState(null);
 
-  const openDetailsModal = (rate) => {
+  const openDetailsModal = (rate: any) => {
     setSelectedRate(rate);
   };
 
@@ -29,136 +29,144 @@ const [rowsPerPage, setRowsPerPage] = React.useState(10);
   };
 
 
- 
+
 
   if (isLoading) {
     return <p>Loading...</p>;
-    }
-    
-     interface Rate {
-   id: string;
-   name: string;
-   priceUsd: Number;
-   rank: Number;
-   symbol: String;
-   currencySymbol: String;
-   type: String;
-   rateUsd: String;
+  }
 
-  
-    }
-    
-    const handleChangePage = (event: unknown, newPage: number) => {
-      setPage(newPage);
-    };
-    
-    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setRowsPerPage(+event.target.value);
-      setPage(0);
-    };
-   
+  interface Rate {
+    id: string;
+    name: string;
+    priceUsd: Number;
+    rank: Number;
+    symbol: String;
+    currencySymbol: String;
+    type: String;
+    rateUsd: String;
+  }
 
-    return (
-        <>
-             <Wrapper>
-    <div>
-          <h1 style={{textAlign: "center"}}>Rate Page</h1>
+  interface selectedRate {
+    currencySymbol: String;
+    rateUsd: String;
+  }
 
-               
-  <Grid container spacing={2}>
-  <Grid item xs={6}>
-  <Button variant="outlined" color="success">
+
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+
+  return (
+    <>
+      <Wrapper>
+        <div>
+          <h1 style={{ textAlign: "center" }}>Rate Page</h1>
+
+
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <Button variant="outlined" color="success">
                 <Link href={'/'}>
-                Back
+                  Back
                 </Link>
-                </Button>
-                    
-  </Grid>
-  <Grid item xs={6}>
-  <Button variant="outlined" color="success">
+              </Button>
+
+            </Grid>
+            <Grid item xs={6}>
+              <Button variant="outlined" color="success">
                 <Link href={'/exchangepages'}>
-                 Next
+                  Next
                 </Link>
-                </Button>
-  </Grid>
-  </Grid>
+              </Button>
+            </Grid>
+          </Grid>
 
 
 
-                    
+
           <TableContainer component={Paper}>
-      <Table>
-                            <TableBody>
-                                
-                                <TableRow>
-           <TableCell>ID</TableCell>
-           <TableCell>SYMBOL</TableCell>
-           <TableCell>CURRENCY_SIGN</TableCell>
-           <TableCell>TYPE</TableCell>
-           <TableCell style={{ color: 'red' }}>RATE($)</TableCell>
-           <TableCell>DETAILS</TableCell>
-              </TableRow>
+            <Table>
+              <TableBody>
 
-              
-                      
-    
-        {rates
-        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-        .map((rate : Rate) => (
-            <TableRow key={rate.id}>
-                <TableCell>{rate.id}</TableCell>
-              <TableCell>
-              <Link href={`ratepages/${rate.id}`}>
-                {rate.symbol}
-                </Link>
-            </TableCell>
-              <TableCell>{rate.currencySymbol}</TableCell>
-                <TableCell>{rate.type}</TableCell>
-                <TableCell  style={{ color: 'red' }}>{rate.rateUsd}</TableCell>
-                <TableCell>
-              
-                <Button onClick={() => openDetailsModal(rate)}>View Details</Button>
-              
-            </TableCell>
-                
-              </TableRow>
-        ))}
-              
-                  </TableBody>
-              </Table>
-              </TableContainer>
+                <TableRow>
+                  <TableCell>ID</TableCell>
+                  <TableCell>SYMBOL</TableCell>
+                  <TableCell>CURRENCY_SIGN</TableCell>
+                  <TableCell>TYPE</TableCell>
+                  <TableCell style={{ color: 'red' }}>RATE($)</TableCell>
+                  <TableCell>DETAILS</TableCell>
+                </TableRow>
 
 
-              <Dialog open={Boolean(selectedRate)} onClose={closeDetailsModal}>
-        <DialogTitle>Rate Details</DialogTitle>
-        <DialogContent>
-          {selectedRate && (
-            <>
-                    <Typography variant="subtitle1">{selectedRate.currencySymbol}</Typography>
-                    <Typography variant="subtitle1">{selectedRate.rateUsd}</Typography>
-              
-            </>
-          )}
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={closeDetailsModal}>Close</Button>
-        </DialogActions>
-      </Dialog>
 
 
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rates.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
+                {rates
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((rate: Rate) => (
+                    <TableRow key={rate.id}>
+                      <TableCell>{rate.id}</TableCell>
+                      <TableCell>
+                        <Link href={`ratepages/${rate.id}`}>
+                          {rate.symbol}
+                        </Link>
+                      </TableCell>
+                      <TableCell>{rate.currencySymbol}</TableCell>
+                      <TableCell>{rate.type}</TableCell>
+                      <TableCell style={{ color: 'red' }}>{rate.rateUsd}</TableCell>
+                      <TableCell>
 
-                </div>
-            </Wrapper>
-        </>
+                        <Button onClick={() => openDetailsModal(rate)}>View Details</Button>
+
+                      </TableCell>
+
+                    </TableRow>
+                  ))}
+
+              </TableBody>
+            </Table>
+          </TableContainer>
+
+
+          <Dialog open={Boolean(selectedRate)} onClose={closeDetailsModal}>
+            <DialogTitle>Rate Details</DialogTitle>
+            <DialogContent>
+
+
+              {selectedRate && (
+
+                <>
+                  <Typography variant="subtitle1">{selectedRate.currencySymbol}</Typography>
+                  <Typography variant="subtitle1">{selectedRate.rateUsd}</Typography>
+
+                </>
+              )}
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={closeDetailsModal}>Close</Button>
+            </DialogActions>
+          </Dialog>
+
+
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={rates.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+
+        </div>
+      </Wrapper>
+    </>
   );
 };
 
